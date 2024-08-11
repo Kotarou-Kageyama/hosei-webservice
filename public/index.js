5e899452420824e66805468ee278cbf13e6de594
Kotarou-Kageyama <kotarou.kageyama.5n@stu.hosei.ac.jp>
2024-08-11 19:42:07 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -7,8 +7,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
   });
 
   document.querySelector('.send-button').addEventListener('click', (event) => {
-    const text = document.querySelector('.input-text').value;
-    fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
+    
+    const month = document.querySelector('.month').value;
+    const day = document.querySelector('.day').value;
+    const hour = document.querySelector('.hour').value;
+    const minute = document.querySelector('.minute').value;
+    const time = "("+month+"月"+day+"日"+hour+"時"+minute+"分"+")";
+    const text = document.querySelector('.input-text').value + time;
 
+    fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text}) })
   });
+  
 });
